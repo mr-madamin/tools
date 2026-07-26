8c0348474e8385d01b5da984c3af311d8b4c381a
Daler <8993696+mr-madamin@users.noreply.github.com>
2026-07-26 14:16:47 +0400

    Add file sender

---
+++ lan_folder_sync/file_sender.py
@@ -0,0 +1,13 @@
+import socket
+import sys
+from framing import send_file
+
+HOST = sys.argv[1] if len(sys.argv) > 1 else "127.0.0.1"
+PORT = 8765
+path = sys.argv[2] if len(sys.argv) > 2 else "bigfile.bin"
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((HOST, PORT))
+send_file(sock, path)
+print(f"Sent {path}")
+sock.close()
